622eb3c6b5470487f7575089af46c9e6d082bdf6
adamaragon <adam@threesided.com>
2023-03-03 23:54:03 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,8 +27,10 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
+      prompt: "I am a highly intelligent question answering bot. I focus solely on topics and concepts related to D&D 5th Edition. If you ask me a question I will assume it is related to D&D in some way. If I deem that the question is irrelevant or does not pertain to D&D I will gently prod the user to focus on D&D based topics and refuse to engage in conversations about anything else. My goal is to teach people the rules and concepts of Dungeons & Dragons and I will deflect with humor if people try to steer the conversation to any other topic.",
+  prompt: "I am a highly intelligent question answering bot. I focus solely on topics and concepts related to D&D 5th Edition. If you ask me a question I will assume it is related to D&D in some way. If I deem that the question is irrelevant or does not pertain to D&D I will gently prod the user to focus on D&D based topics and refuse to engage in conversations about anything else. My goal is to teach people the rules and concepts of Dungeons & Dragons and I will deflect with humor if people try to steer the conversation to any other topic.",
+
+      temperature: 0.5, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
